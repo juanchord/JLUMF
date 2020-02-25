78d2740fc795e4ebf0c8bc566712fa0c8866a5c7
Leyms <leyra017@hotmail.com>
2020-02-25 18:53:53 -0400

    Vistas y controladores Listos

---
+++ Tarea01/Tarea01/Models/Persona.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Tarea01.Models
+{
+    public class Persona
+    {
+         
+        [Required(ErrorMessage = "La cédula es Obligatoria")]
+        public string Cedula { get; set; }
+        [Required(ErrorMessage = "El nombre es Obligatorio")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "El apellido es Obligatorio")]
+        public string Apellido { get; set; }
+        [Range(15, 100, ErrorMessage = "Debes ser mayor a 15 años")]
+        public string Edad { get; set; }
+        public string Telefono { get; set; }
+        [Required(ErrorMessage = "Correo es Requerido")]
+        [DataType(DataType.EmailAddress)]
+        [MaxLength(50)]
+        [RegularExpression(@"[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}", ErrorMessage = "Formato Incorrecto")]
+        public string CorreoElectronico { get; set; }
+        public Puestoo Puesto { get; set; }
+        public string Genero { get; set; }
+    }
+
+    public enum Puestoo
+    {
+        Gerente, SubGerente, Asistente, Colaborador, Temporero
+    }
+
+
+}
+
